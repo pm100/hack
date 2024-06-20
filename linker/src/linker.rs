3c4dc85a4d9a03fd69df9e391ebe70d080b0c61a
paul moore <paulmoore100@hotmail.com>
2024-06-19 18:31:45 -0700

    clenup linker

--- linker/src/linker.rs
+++ linker/src/linker.rs
@@ -1,19 +1,16 @@
 use std::{
     cell::RefCell,
-    collections::{BTreeMap, HashMap, HashSet},
-    fs::{self, File},
+    collections::{BTreeMap, HashSet},
+    fs::File,
 };
 
 use anyhow::{bail, Result};
-use pest::{iterators::Pair, Parser};
 use std::io::Write;
-#[derive(pest_derive::Parser)]
-#[grammar = "../../vcomp/src/vm.pest"]
-struct VMParser {}
 
 pub struct Linker {
     chunks: BTreeMap<String, Chunk>,
     emitted: RefCell<HashSet<String>>,
+    verbose: bool,
 }
 struct Chunk {
     name: String,
@@ -21,27 +18,28 @@ struct Chunk {
     calls: Vec<String>,
 }
 impl Linker {
-    pub fn new() -> Self {
+    pub fn new(verbose: bool) -> Self {
         Self {
             chunks: BTreeMap::new(),
             emitted: RefCell::new(HashSet::new()),
+            verbose,
         }
     }
-    pub fn load(&mut self, source: &String, name: &str) -> Result<()> {
+    pub fn load(&mut self, source: &str) -> Result<()> {
+        // incoming file is chopped up into chunks , each chunk is a function
+        // for each chunk we record what functions it calls
         let lines = source.lines();
-
         let mut current_function_name = String::new();
         for line in lines {
-            //println!("{}", line);
             let mut parts = line.split_whitespace();
-            // allow for blank lines
             if let Some(command) = parts.next() {
                 match command {
                     "function" => {
                         let func_name = parts.next().unwrap().to_string();
-                        let _locals = parts.next().unwrap();
-                        println!("{}", line);
-
+                        if self.verbose {
+                            println!("Function {}", func_name)
+                        };
+                        // start new chunk
                         current_function_name = func_name.clone();
                         let new_chunk = Chunk {
                             name: func_name.to_string(),
@@ -54,22 +52,22 @@ impl Linker {
                         }
                     }
                     "call" => {
+                        // record the call
                         let name = parts.next().unwrap();
-                        println!("   call {}", name);
-                        let args = parts.next().unwrap();
+                        if self.verbose {
+                            println!("   call {}", name)
+                        };
                         if let Some(ref mut chunk) = self.chunks.get_mut(&current_function_name) {
                             chunk.calls.push(name.to_string());
                         } else {
                             bail!("call without function");
                         }
                     }
-                    "return" => {
 
-                        // continue;
-                    }
                     _ => {}
                 }
             }
+            // store each line in the current chunk
             if let Some(ref mut chunk) = self.chunks.get_mut(&current_function_name) {
                 chunk.code.push(line.to_string());
             } else {
@@ -80,32 +78,53 @@ impl Linker {
         Ok(())
     }
     pub fn output_code(&mut self, name: &str) -> Result<()> {
+        // we now have a complete tree of function calls and definitions
         let mut output = File::create(name)?;
-        for chunk in self.chunks.values() {
-            println!("{}:", chunk.name);
-            for call in &chunk.calls {
-                println!(" =>{}", call);
+        if self.verbose {
+            println!("Generated call tree---------------");
+            for chunk in self.chunks.values() {
+                println!("{}:", chunk.name);
+                for call in &chunk.calls {
+                    println!(" =>{}", call);
+                }
             }
+
+            println!("Emitting code---------------");
         }
-        self.emit("Sys.init", &mut output)?;
-        // self.emit("Main.main", &mut output)?;
+        // Sys.init is the root of the call tree
+
+        self.emit("Sys.init", &mut output, 0)?;
         Ok(())
     }
 
-    fn emit(&self, name: &str, output: &mut File) -> Result<()> {
+    fn emit(&self, name: &str, output: &mut File, depth: i8) -> Result<()> {
+        // recurse down the call tree satisfying each function call we hit
+
+        // did we already emit this function?
+
         if self.emitted.borrow().contains(name) {
             return Ok(());
         }
         self.emitted.borrow_mut().insert(name.to_string());
+
+        if self.verbose {
+            for _i in 0..depth {
+                print!("  ");
+            }
+            println!("emitting {}", name);
+        }
         if let Some(chunk) = self.chunks.get(name) {
+            // include the fiunctions code
+
             for line in &chunk.code {
                 writeln!(output, "{}", line)?;
             }
+            // include the code for each function it calls
             for call in &chunk.calls {
-                self.emit(call, output)?;
+                self.emit(call, output, depth + 1)?;
             }
         } else {
-            println!("unrsolved external reference: {}", name);
+            bail!("Unresolved external reference: {}", name);
         }
         Ok(())
     }
--- linker/src/main.rs
+++ linker/src/main.rs
@@ -1,5 +1,19 @@
+/*
+   A 'linker' for the nand 2 tetris toolchain
+
+   It reads in a Main.vm file and then resolves all 'os' calls
+   by merging in the relevant functions from
+   - the supplied directory
+   - the supplied library (assumed to point to the sstandard OS functions)
+
+   This produces a single large vm file that can then be compiled and assembled
+
+   It produces a much smaller binary as only the needed code is merged in
+
+*/
+
 mod linker;
-use anyhow::{anyhow, bail, Result};
+use anyhow::{bail, Result};
 
 use clap::Parser;
 use clap_derive::Parser;
@@ -17,14 +31,16 @@ struct Args {
     outfile: Option<PathBuf>,
     #[arg(short, long)]
     lib: Option<PathBuf>,
+    #[arg(short, long)]
+    verbose: bool,
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    let cur = std::env::current_dir()?;
-    println!("{:?}", cur);
-    let input_path = args.input;
+    let verbose = args.verbose;
 
+    let input_path = args.input;
+    // the input points at a directory
     if input_path.is_file() {
         bail!("Input must be a directory");
     }
@@ -41,48 +57,45 @@ fn main() -> Result<()> {
         .as_os_str()
         .to_str()
         .unwrap();
-
+    // the generated file name is <dir>.vm by default
     let output_name = if let Some(outfile) = args.outfile {
         outfile.to_str().unwrap().to_string()
     } else {
         format!("{}.vm", name)
     };
 
-    fs::remove_file(&output_name).ok(); // ignore errors
-                                        // input points at Main.vm
+    // delete the previous output so we dont try to include in the new run
+    fs::remove_file(&output_name).ok();
 
-    let main_path = input_path.join("Main.vm");
-    println!("{:?}", main_path);
-    if !main_path.exists() {
+    // make sure Main.vm exists
+
+    if !input_path.join("Main.vm").exists() {
         bail!("Main.vm not found");
     }
 
-    let mut linker = Linker::new();
-    //  linker.load(&fs::read_to_string(&main_path)?, "Main")?;
-    load_dir(input_path.as_path(), &mut linker, &main_path)?;
-
+    let mut linker = Linker::new(verbose);
+    load_dir(input_path.as_path(), &mut linker, verbose)?;
     if let Some(oslib) = args.lib {
-        load_dir(oslib.as_path(), &mut linker, &main_path)?;
+        load_dir(oslib.as_path(), &mut linker, verbose)?;
     }
     linker.output_code(&output_name)?;
-
     Ok(())
 }
-fn load_dir(path: &Path, linker: &mut Linker, main_path: &PathBuf) -> Result<()> {
+
+fn load_dir(path: &Path, linker: &mut Linker, verbose: bool) -> Result<()> {
     for entry in fs::read_dir(path)? {
         let entry = entry?;
         let path = entry.path();
 
         if path.is_file() {
-            // if path == main_path.as_path() {
-            //     continue;
-            // }
             if let Some(ftype) = path.extension() {
                 if ftype.to_str().unwrap() == "vm" {
-                    println!("loading {:?}", path);
+                    if verbose {
+                        println!("loading {:#?}", path)
+                    };
                     let source = fs::read_to_string(path.clone())?;
-                    let name = path.file_stem().unwrap().to_str().unwrap();
-                    linker.load(&source, name)?;
+                    // let name = path.file_stem().unwrap().to_str().unwrap();
+                    linker.load(&source)?;
                 }
             }
         }
