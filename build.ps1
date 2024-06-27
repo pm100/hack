6585216c7428b97af097cd43d4c4a4c3d2d0b0c5
paul moore <paulmoore100@hotmail.com>
2024-06-26 18:05:28 -0700

    cleanup build.ps1

--- build.ps1
+++ build.ps1
@@ -1,11 +1,18 @@
+if (($args.Length -gt 0) -And ($args[0] -eq "-v")) {$verb = "-v"} else {$verb = ""}
+echo $args, $verb
 $dir = split-path $pwd -leaf
 $jack_files = Get-ChildItem -Path . -Filter *.jack
 foreach ($j in $jack_files) {
-    $j
-    C:\work\hack\target\debug\jcomp.exe -i $j
+    echo "Compiling $j"
+    C:\work\hack\target\debug\jcomp.exe -i $j $verb
+    if ($LASTEXITCODE -ne 0) {
+        exit $LASTEXITCODE
+    }
 }
-
-C:\work\hack\target\debug\linker.exe -i . --lib C:\work\hack\os\ 
-C:\work\hack\target\debug\vcomp.exe -i .\$dir.vm -a
-C:\work\hack\target\debug\assembler.exe -i .\$dir.asm -f hx -l .\$dir.lst -o .\$dir.hx
-C:\work\hack\target\debug\assembler.exe -i .\$dir.asm -f b -l .\$dir.lst -o .\$dir.hack
+echo "Linking"
+C:\work\hack\target\debug\linker.exe -i . --lib C:\work\hack\os\ $verb
+C:\work\hack\target\debug\vcomp.exe -i .\$dir.vm -a $verb
+echo "Assembling for Hackem"
+C:\work\hack\target\debug\assembler.exe -i .\$dir.asm -f hx -l .\$dir.lst -o .\$dir.hx $verb
+echo "Assembling for CPUEmulator"
+C:\work\hack\target\debug\assembler.exe -i .\$dir.asm -f b   -o .\$dir.hack $verb
